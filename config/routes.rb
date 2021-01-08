858017f0491b861093f02473b39d4845940de016
Monica Li <monicali@live.ca>
2021-01-08 05:35:24 +0000

    adds contoller for user and sesssion, creates new view for signup

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   namespace :admin do
     root to: 'dashboard#show'
