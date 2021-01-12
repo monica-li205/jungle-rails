fbc022884161fdad7970c24d53f169728e55dad9
Monica Li <monicali@live.ca>
2021-01-12 03:37:46 +0000

    adds save and check error messages to rspec tests

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -4,36 +4,45 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here 
     category = Category.new(:name => "anything")
-    subject { 
-      described_class.new(
-        name: "anything",
-        price_cents: 1,
-        quantity: 1,
-        category: category)
-    }
+    product = Product.new(
+      :name => "anything",
+      :price_cents => 1,
+      :quantity => 1,
+      :category => category)
 
     it "is valid with valid attributes" do
-      expect(subject).to be_valid
+      expect(product).to be_valid
+      product.save
     end
 
     it "is has a name" do
-      subject.name = nil
-      expect(subject).to_not be_valid
+      product.name = nil
+      product.save
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
     end
 
     it "is has a price" do
-      subject.price_cents = nil
-      expect(subject).to_not be_valid
+      product.price_cents = nil
+      product.save
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Price is not a number")
     end
 
     it "is has a quantity" do
-      subject.quantity = nil
-      expect(subject).to_not be_valid
+      product.quantity = nil
+      product.save
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
+
     end
 
     it "is has a category" do
-      subject.category = nil
-      expect(subject).to_not be_valid
+      product.category = nil
+      product.save
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Category can't be blank")
+
     end
   end
 end
