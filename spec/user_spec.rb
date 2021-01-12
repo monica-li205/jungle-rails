4021a28150bc8e00c038506524e4c9d7f04d0a32
Monica Li <monicali@live.ca>
2021-01-12 05:08:32 +0000

    adds test for valid first name

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,18 +7,37 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    
+
     user = User.new(
-      first_name:,
-      last_name:,
-      email:,
-      password_digest:)
+      first_name: "Anything",
+      last_name:"Anything",
+      email: "Anything@Anything.com",
+      password_digest: "password")
     
     # validation tests/examples here
-    it 'has a valid password and password confirmation' do
+    it 'is valid with valid attributes' do
+      expect(user).to be_valid
+      user.save
     end
-    it 'has a unique email'
-    it 'has a valid email'
-    it
+
+    it 'has a valid password and password confirmation is present' do
+      
+    end
+    
+    it 'has a unique email' do
+    end
+
+    it 'has a valid email' do
+    end
+
+    it 'has a valid first name' do
+      user.first_name = nil
+      expect(user).to_not be_valid
+      user.save
+    end
+
+    it 'has a valid last name' do
+    end
+
   end
 end
