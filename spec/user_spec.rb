2de9ed5aaa3d30569dfa384867e56429f0c9e82d
Monica Li <monicali@live.ca>
2021-01-12 03:13:37 +0000

    adds some tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -9,11 +9,16 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     subject {
       described_class.new(
-        first_name
-        last_name
-      )
+        first_name:,
+        last_name:,
+        email:,
+        password_digest:)
     }
     # validation tests/examples here
-    it ''
+    it 'has a valid password and password confirmation' do
+    end
+    it 'has a unique email'
+    it 'has a valid email'
+    it
   end
 end
